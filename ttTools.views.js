eb94feea56e6fe3b892a1406a1a1f21c95da072e
Gavin P. Hogan <gavin@buyspace.com>
2012-08-18 19:13:34 -0400

    trying some different locations for button bar

--- ttTools.views.js
+++ ttTools.views.js
@@ -172,9 +172,9 @@ div#playlistTools .custom-icons.dice { background-position:17px 0; }\
 div#playlistTools .custom-icons.soundcloud { background-position:34px 0; }\
       "}).appendTo(document.head);
 
-      $('.share-on').children.remove()
+      $('.share-on').children().remove()
       $(util.buildTree(this.tree())).insertBefore(
-        $('#playlist .black-right-header')
+        $('.share-on')
       );
 
       $('div#buttons').buttonset();
